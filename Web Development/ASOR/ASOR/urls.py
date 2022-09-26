79b707d870d015fa9e449a3eec911e1df1a4fe94
Christopher Lakey <christopher@wildforgedev.com>
2022-09-25 22:01:18 -0600

    Fixing a bunch of stuff with URL

--- Web Development/ASOR/ASOR/urls.py
+++ Web Development/ASOR/ASOR/urls.py
@@ -1,3 +1,12 @@
+# from django.contrib import admin
+# from django.urls import path, include
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', include('main.urls')),
+#     path('', include("django.contrib.auth.urls")),
+# ]
+
 from django.contrib import admin
 from django.urls import path, include
 
